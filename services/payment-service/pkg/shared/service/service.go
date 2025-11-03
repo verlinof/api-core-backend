4421b227547497c529c5aaf4bbb1134e72d192df
verlinof <ajikoko.zx@gmail.com>
2025-11-03 09:42:03 +0700

    Feat<notifier>: Add webhook for Logistik API to receive notification

--- services/payment-service/pkg/shared/service/service.go
+++ services/payment-service/pkg/shared/service/service.go
@@ -3,16 +3,19 @@ package service
 import (
 	"payment-service/pkg/shared"
 	mid_trans "payment-service/pkg/shared/service/midtrans"
+	"payment-service/pkg/shared/service/notifier"
 )
 
 const KeyExternalService string = "midtrans-external-service"
 
 type serviceImpl struct {
 	midtransService mid_trans.Midtrans
+	notifierService notifier.Notifier
 }
 
 type Service interface {
 	Midtrans() mid_trans.Midtrans
+	Notifier() notifier.Notifier
 }
 
 func New() Service {
@@ -20,9 +23,14 @@ func New() Service {
 		midtransService: mid_trans.NewMidtransService(
 			shared.GetEnv().MidtransEnv, shared.GetEnv().MidtransServerKey,
 		),
+		notifierService: notifier.NewNotifierService(),
 	}
 }
 
 func (s *serviceImpl) Midtrans() mid_trans.Midtrans {
 	return s.midtransService
+}
+
+func (s *serviceImpl) Notifier() notifier.Notifier {
+	return s.notifierService
 }
